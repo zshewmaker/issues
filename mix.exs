899201d5550b7a5252812cf9baeaaf05dc182e38
Zach Shewmaker <zach.shewmaker@zapproved.com>
2021-09-25 14:38:16 -0700

    Pre docs

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
@@ -21,8 +22,12 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.25.3"},
+      {:earmark, "~> 1.4"},
     ]
   end
+
+  defp escript_config(), do: [main_module: Issues.CLI]
 end
